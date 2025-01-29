99038c37df9ddd4b7ad082d0ff90940f167d9198
Tatum McKinnis <tam85@duke.edu>
2025-01-28 20:44:26 -0500

    Feature CELL-01: Add State for Game of Life Simulation

---
+++ src/main/java/cellsociety/Model/State/GameOfLifeState.java
@@ -0,0 +1,19 @@
+/**
+    * Enum representing the states for Conway's Game of Life simulation.
+    * <p>
+ * Conway's Game of Life has two possible states: ALIVE and DEAD. A cell can either be alive
+    * or dead based on the number of neighbors it has in each generation.
+ * </p>
+    */
+public enum GameOfLifeState {
+
+  /**
+   * Represents a cell that is alive in Conway's Game of Life.
+   */
+  ALIVE,
+
+  /**
+   * Represents a cell that is dead in Conway's Game of Life.
+   */
+  DEAD
+}
