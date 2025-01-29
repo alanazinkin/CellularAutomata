9a478ad62845077893c7a3313e263a8321ad7797
Tatum McKinnis <tam85@duke.edu>
2025-01-28 20:44:58 -0500

    Feature CELL-02: Add State for Percolation Simulation

---
+++ src/main/java/cellsociety/Model/State/PercolationState.java
@@ -0,0 +1,19 @@
+/**
+ * Enum representing the states for the Percolation simulation.
+ * <p>
+ * Percolation simulation has two possible states: OPEN and BLOCKED. A cell can be open (allowing
+ * fluid to flow through it) or blocked (impeding the flow of fluid).
+ * </p>
+ */
+public enum PercolationState {
+
+  /**
+   * Represents an open cell in the Percolation simulation, allowing fluid to flow through it.
+   */
+  OPEN,
+
+  /**
+   * Represents a blocked cell in the Percolation simulation, which blocks the flow of fluid.
+   */
+  BLOCKED
+}
