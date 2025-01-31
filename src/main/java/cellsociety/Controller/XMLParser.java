9e1c2b5e820af2ae6d3b457066f33b51c9ff24d1
angela <ap721@duke.edu>
2025-01-31 18:51:40 -0500

    Feature: worked on XMLParser class

--- src/main/java/cellsociety/Controller/XMLParser.java
+++ src/main/java/cellsociety/Controller/XMLParser.java
@@ -1,6 +1,39 @@
 package cellsociety.Controller;
 
+import org.w3c.dom.Document;
+import javax.xml.parsers.*;
+import java.io.*;
+
+
 public class XMLParser {
 
+    public SimulationConfig parseXMLFile(String filePath) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File(filePath));
+        document.getDocumentElement().normalize();
+
+        String type = getElementContent(document, "type");
+        String title = getElementContent(document, "title");
+        String author = getElementContent(document, "author");
+        String description = getElementContent(document, "description");
+        int width = Integer.parseInt(getElementContent(document, "width"));
+        int height = Integer.parseInt(getElementContent(document, "height"));
+
+        String[] statesStr = getElementContent(document, "initial_states").split("\\s+");
+        int[] initialStates = new int[statesStr.length];
+        for (int i = 0; i < statesStr.length; i++) {
+            initialStates[i] = Integer.parseInt(statesStr[i]);
+        }
+
+        SimulationParameter parameters = new SimulationParameter();
+        
+        return new SimulationConfig(type, title, author, description, width, height,
+                initialStates, parameters);
+    }
+
+    private String getElementContent(Document doc, String tagName) {
+        return doc.getElementsByTagName(tagName).item(0).getTextContent();
+    }
 
 }
---
+++ src/test/java/cellsociety/Controller/SimulationConfigTest.java
@@ -0,0 +1,40 @@
+package cellsociety.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimulationConfigTest {
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getAuthor() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getInitialStates() {
+    }
+
+    @Test
+    void getParameter() {
+    }
+}
