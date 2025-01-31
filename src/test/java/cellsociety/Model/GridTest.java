4e3d701d6da28f0d3f194721fbaa946499fc36fd
Tatum McKinnis <tam85@duke.edu>
2025-01-30 21:36:56 -0500

    Test DESIGN-12: Create Test classes for Cell and Grid

---
+++ src/test/java/cellsociety/Model/GridTest.java
@@ -0,0 +1,36 @@
+package cellsociety.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GridTest {
+
+  @Test
+  void getCell() {
+  }
+
+  @Test
+  void getNeighbors() {
+  }
+
+  @Test
+  void applyNextStates() {
+  }
+
+  @Test
+  void resetGrid() {
+  }
+
+  @Test
+  void printGrid() {
+  }
+
+  @Test
+  void getRows() {
+  }
+
+  @Test
+  void getCols() {
+  }
+}
