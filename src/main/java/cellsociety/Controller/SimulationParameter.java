641008f1cb45f04b606f4c590f2a0aba64feba4e
angela <ap721@duke.edu>
2025-01-31 18:23:24 -0500

    Feature: Created XMLParser and SimulationParameter classes for Controller

---
+++ src/main/java/cellsociety/Controller/SimulationParameter.java
@@ -0,0 +1,11 @@
+package cellsociety.Controller;
+
+public class SimulationParameter {
+
+    /**
+     * Empty constructor because Game of Life Simulation has no parameters
+     */
+    public SimulationParameter() {
+
+    }
+}
---
+++ src/main/java/cellsociety/Controller/XMLParser.java
@@ -0,0 +1,6 @@
+package cellsociety.Controller;
+
+public class XMLParser {
+
+
+}
