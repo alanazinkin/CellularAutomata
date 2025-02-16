00996f4dd1d53c6733c9d42c054f87e13dc76593
Tatum McKinnis <tam85@duke.edu>
2025-02-16 09:07:27 -0500

    Refactor DESIGN-02: update package names to lowercase - relates to #50

--- src/main/java/cellsociety/View/gridviews/GameOfLifeGridView.java
+++ src/main/java/cellsociety/View/gridviews/GameOfLifeGridView.java
@@ -4,15 +4,7 @@ import static cellsociety.View.SimulationView.SIMULATION_HEIGHT;
 import static cellsociety.View.SimulationView.SIMULATION_WIDTH;
 
 import cellsociety.Controller.SimulationConfig;
-import cellsociety.Model.Cell;
 import cellsociety.Model.Grid;
-import cellsociety.Model.State.GameOfLifeState;
-import cellsociety.Model.StateInterface;
-import java.util.ArrayList;
-import java.util.Map;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Shape;
 
 public class GameOfLifeGridView extends GridView {
 
