1b1a4d4be6ad48dde03613b3b872a1e9a51f370f
angela <ap721@duke.edu>
2025-03-02 20:43:26 -0500

    CELL-47: Added CellAppearance class

---
+++ src/main/java/cellsociety/controller/CellAppearance.java
@@ -0,0 +1,40 @@
+package cellsociety.controller;
+
+/**
+ * Represents the appearance of a cell state.
+ * @author angelapredolac 
+ */
+public class CellAppearance {
+    private String color;
+    private String imagePath;
+
+    public CellAppearance() {
+        this.color = null;
+        this.imagePath = null;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    /**
+     * Determines if this cell appearance uses an image.
+     *
+     * @return true if an image path is specified, false otherwise.
+     */
+    public boolean usesImage() {
+        return imagePath != null && !imagePath.isEmpty();
+    }
+}
