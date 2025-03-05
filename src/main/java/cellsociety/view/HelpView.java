3f9541d42c905d8bdf52ba8e3d97af6eca013a66
Alana Zinkin <abz8@duke.edu>
2025-03-04 22:11:45 -0500

    Feature CELL-44: create Help documentation view - closes #92

--- src/main/java/cellsociety/view/HelpView.java
+++ src/main/java/cellsociety/view/HelpView.java
@@ -1,4 +1,35 @@
 package cellsociety.view;
 
+import static java.lang.Integer.parseInt;
+import java.util.ResourceBundle;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import java.io.File;
+
+/**
+ * Help View displays an HTML help view for each simulation
+ */
 public class HelpView {
+
+  private static final File helpFile = new File("src/main/resources/cellsociety/view/help.html");
+  private static final ResourceBundle helpResources = ResourceBundle.getBundle(
+      HelpView.class.getPackage().getName() + ".help");
+
+  /**
+   * opens up a help documentation window providing information about the program to assist users
+   */
+  public void showHelpWindow() {
+    Stage helpStage = new Stage();
+    helpStage.setTitle("Simulation Help");
+
+    WebView webView = new WebView();
+    webView.getEngine().load(helpFile.toURI().toString());
+
+    Scene scene = new Scene(webView, parseInt(helpResources.getString("help.window.width")),
+        parseInt(helpResources.getString("help.window.height")));
+    helpStage.setScene(scene);
+    helpStage.show();
+  }
 }
+
